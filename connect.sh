caf28ceb5e3290a7dbc5e43cba7c459e067004f8
Mike Carifio <mike@carif.io>
2020-02-05 22:48:20 -0500

    Added local `me` with absolute pathname.

--- connect.sh
+++ connect.sh
@@ -1,12 +1,14 @@
+# bashdb -c 'source connect.sh'
 _main() {
-    local here=${BASH_SOURCE%/*}
+    local me=$(realpath ${BASH_SOURCE})
+    local here=${me%/*}
     local root=$(git -C ${here} rev-parse --show-toplevel)
     local parent=$(git -C ${root}/.. rev-parse --show-toplevel)
     local relpath=$(realpath ${root} --relative-to=${parent})
 
-    echo -e "# added by '${BASH_SOURCE}' on '$(date)'\n/${relpath}/**" >> ${parent}/.git/info/exclude
+    echo -e "# added by '${me}' on '$(date)'\n/${relpath}/**" >> ${parent}/.git/info/exclude
     PATH="${here}/bin:$PATH"
 }
 
-_main()
+_main
 unset _main
